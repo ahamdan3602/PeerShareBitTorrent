f3d579a78f97703ed1951d23b4ddbb00ecd85c47
ahamdan3602 <ahamdan4@uwo.ca>
2026-06-27 16:41:42 -0400

    chore: complete bencode parsing/decoding:write unit test for Client

--- src/Bencode.java
+++ src/Bencode.java
@@ -1,59 +1,81 @@
 import java.lang.invoke.WrongMethodTypeException;
 
-import com.google.gson.Gson;
-import com.dampcake.bencode.Bencode; //- available if you need it!
-
 import java.util.*;
 
 
 public class Bencode {
+    private final String input;
+    private int pos = 0;
 
-    public static Object decodeBencode(bencodedString) { 
-        
-        // Decoding bencoded strings
-        // Byte strings are encoded as follows:
-        // <string length encoded in base ten ASCII>:<string data>
-        if (Character.isDigit(bencodedString.charAt(0))) {
-            int colon_index = 0;
-            for (int i = 0; i < bencodedString.length();i++) {
-                if (bencodedString.charAt(i)== ':') {
-                    colon_index = i;
-                    break;
-                }
-            }
-            int length = Integer.parseInt(bencodedString.substring(0, colon_index));
-            return bencodedString.substring(colon_index, colon_index+1+length);
-        } 
-        // Decoding bencoded integers
-        // Integers are encoded as follows:
-        // i<integer encoded in base ten ASCII>e
-        else if (bencodedString.startsWith("i")) {
-            return Long.parseLong(bencodedString.substring(1, bencodedString.indexOf('e')));
-        } 
-        // Decoding bencoded lists
-        //Lists are encoded as follows: 
-        // l<bencoded values>e
-        else if (bencodedString.startsWith("l")) {
-            List<Object> arr = new ArrayList<>(); 
-            for (int i = 0; i < bencodedString.length();i++) {
-                // handle string in list
-                if (Character.isDigit(bencodedString.charAt(i))) {
-                    int colonIndex = bencodedString.indexOf(':', i);
-                    int length = Integer.parseInt(bencodedString.substring(i, colon_index));
-                    arr.add(bencodedString.substring(colon_index+1, (colon_index+1) + length));
-                    i = ((colonIndex + 1) + length) - 1;
-                } 
-                // handle int in list
-                else if (bencodedString.charAt(i) == 'i') {}
-            }
-        }
-
-
-
-
+    private Bencode(String input) {
+        this.input = input;
     }
 
+ 
+    private Object decodeNext() {
+        char current = input.charAt(pos);
 
+        if (Character.isDigit(current)) {
+            return decodeString();
+        }
 
+        if (current == 'i') {
+            return decodeInteger();
+        }
 
+        if (current == 'l') {
+            return decodeList();
+        }
+
+        throw new RuntimeException("Unsupported bencode value");
+    }
+
+    // Decoding bencoded integers
+    // Integers are encoded as follows:
+    // i<integer encoded in base ten ASCII>e
+    private Object decodeInteger() {
+        int end_index = input.indexOf('e', pos);
+        int value = Integer.parseInt(input.substring(pos + 1, end_index));
+
+        pos = end_index + 1;
+
+        return value;
+    }
+
+    // Decoding bencoded strings
+    // Byte strings are encoded as follows:
+    // <string length encoded in base ten ASCII>:<string data>
+    private Object decodeString() {
+       int colon_index = input.indexOf(':', pos);
+
+        int length = Integer.parseInt(input.substring(pos, colon_index));
+
+        int start = colon_index + 1;
+        int end = start + length;
+
+        pos = end;
+        
+        return input.substring(colon_index+1, colon_index + 1 + length);
+    }
+
+    // Decoding bencoded lists
+    // Lists are encoded as follows:
+    // l<bencoded values>e
+    private Object decodeList() {
+        List<Object> arr = new ArrayList<>();
+        pos++;
+
+        while (input.charAt(pos) != 'e') {
+            arr.add(decodeNext());
+        }
+
+        pos++;
+        return arr;
+    }
+
+    public static Object decodeBencode(String bencodedString) {
+
+        Bencode parser = new Bencode(bencodedString);
+        return parser.decodeNext();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         String bencodedValue = args[1];
         Object decoded;
         try {
-          decoded = decodeBencode(bencodedValue);
+          decoded = Bencode.decodeBencode(bencodedValue);
         } catch(RuntimeException e) {
           System.out.println(e.getMessage());
           return;
@@ -30,45 +30,5 @@ public class Main {
 
   }
 
-  
-  static Object decodeBencode(String bencodedString) {
-    // 3:cat
-    if (Character.isDigit(bencodedString.charAt(0))) { //checks if first character is a digit
-      //first character in bencode usually tells us the length of the string
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i; //find the colon that seperates the number from the actual string.
-          break;
-        }
-      }
-      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex)); //extract length of string from the bencode
-      //ex: 3:cat, we get length = 3;
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-      //returns the decoded bencode string, which would be in this case cat.
-    } else if (bencodedString.startsWith("i")) {
-      return Long.parseLong(bencodedString.substring(1, bencodedString.indexOf("e")));
-    } else if (bencodedString.startsWith("l")) {
-        ArrayList<Object> arr = new ArrayList<>();
-
-        for (int i = 1; i < bencodedString.length();i++) {
-          if (bencodedString.charAt(i) == 'i') {
-            arr.add(Long.parseLong(bencodedString.substring(i+1, bencodedString.indexOf('e', i))));
-            i = bencodedString.indexOf("e", i);
-
-        } else if (Character.isDigit(bencodedString.charAt(i))) {
-            int colonIndex = bencodedString.indexOf(':', i);
-            int length = Integer.parseInt(bencodedString.substring(i, colonIndex));
-            arr.add(bencodedString.substring(colonIndex + 1, (colonIndex + 1) + length));
-            i = ((colonIndex + 1) + length) - 1;
-          } else if (bencodedString.charAt(i) == 'l') {
-            
-          }
-        } 
-        return arr;
-    } else {
-      throw new RuntimeException("Only strings are supported at the moment");
-    }
-  }
   
 }
